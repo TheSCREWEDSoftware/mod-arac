b1bee5fb4b4ba647912d9d05252d37f3c55e4b95
FlyingArowana <TheSCREWEDSoftware@users.noreply.github.com>
2026-04-05 10:37:57 +0100

    Backticks

--- data/sql/(Optional)/Drop Template for Custom Race.sql
+++ data/sql/(Optional)/Drop Template for Custom Race.sql
@@ -9,15 +9,15 @@ Class = 12
 
 */
 
-SELECT race + 11 INTO @race FROM `playercreateinfo` WHERE race = 1 LIMIT 1; -- @race
+SELECT `race` + 11 INTO @race FROM `playercreateinfo` WHERE `race` = 1 LIMIT 1; -- @race
 
-SELECT racemask + 2047 INTO @raceMask FROM `playercreateinfo_spell_custom` WHERE racemask = 1 LIMIT 1; -- @raceMask
+SELECT `racemask` + 2047 INTO @raceMask FROM `playercreateinfo_spell_custom` WHERE `racemask` = 1 LIMIT 1; -- @raceMask
 
-SELECT class + 11 INTO @class FROM `player_class_stats` WHERE Class = 1 LIMIT 1; -- @class
+SELECT `class` + 11 INTO @class FROM `player_class_stats` WHERE `Class` = 1 LIMIT 1; -- @class
 
-DELETE FROM `playercreateinfo` WHERE race = @race;
-DELETE FROM `player_race_stats` WHERE Race = @race;
-DELETE FROM `player_class_stats` WHERE Class = @class;
-DELETE FROM `playercreateinfo_action` WHERE race = @race;
-DELETE FROM `playercreateinfo_item` WHERE race = @race;
-DELETE FROM `playercreateinfo_spell_custom` WHERE racemask = @raceMask;
+DELETE FROM `playercreateinfo` WHERE `race` = @race;
+DELETE FROM `player_race_stats` WHERE `Race` = @race;
+DELETE FROM `player_class_stats` WHERE `Class` = @class;
+DELETE FROM `playercreateinfo_action` WHERE `race` = @race;
+DELETE FROM `playercreateinfo_item` WHERE `race` = @race;
+DELETE FROM `playercreateinfo_spell_custom` WHERE `racemask` = @raceMask;
