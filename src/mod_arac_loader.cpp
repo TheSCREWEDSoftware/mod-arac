b4547b60f51aa3763bd80757e2de1784c40d8303
FlyingArowana <TheSCREWEDSoftware@users.noreply.github.com>
2026-04-05 11:10:43 +0100

    Turn into in a module
    Shows on cmake, vs, runtime and autoupdates if enabled

---
+++ src/mod_arac_loader.cpp
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
+ */
+
+//  Show as "mod-arac" in CMake and auto-apply the "data\sql\%db%" if auto-updater enabled.
+void Addmod_aracScripts()
+{
+
+}
